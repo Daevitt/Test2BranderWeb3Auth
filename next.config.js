37e9e63439383a470fe891486d196ff8119a3087
Daevitt <david.eternalsgaming@gmail.com>
2025-09-04 13:47:50 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-}
-module.exports = nextConfig
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    esmExternals: 'loose'
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        url: false,
+        zlib: false,
+        http: false,
+        https: false,
+        assert: false,
+        os: false,
+        path: false,
+      };
+    }
+    
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    
+    return config;
+  },
+  transpilePackages: [
+    '@web3auth/no-modal',
+    '@web3auth/openlogin-adapter', 
+    '@web3auth/ethereum-provider',
+    '@web3auth/base'
+  ]
+};
+
+module.exports = nextConfig;
